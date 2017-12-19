13e0c84e5cceb0b1391e277e2711396c86272b4e
ausbyn <34685189+ausbyn@users.noreply.github.com>
2017-12-19 09:12:18 -0600

    Add files via upload

---
+++ 302_redirects.js
@@ -0,0 +1,21 @@
+﻿if (Modernizr.history    &&
+    Modernizr.svg        &&
+    Modernizr.inlinesvg  &&
+    Modernizr.svgfilters &&
+    Modernizr.svgclippaths) {
+
+    //console.debug("Feature - history      : yes");
+    //console.debug("Feature - svg          : yes");
+    //console.debug("Feature - inlinesvg    : yes");
+    //console.debug("Feature - svgfilters   : yes");
+    //console.debug("Feature - svgclippaths : yes");
+} else {
+
+    //console.log("Feature - history      : no");
+    //console.log("Feature - svg          : no");
+    //console.log("Feature - inlinesvg    : no");
+    //console.log("Feature - svgfilters   : no");
+    //console.log("Feature - svgclippaths : no");
+
+    window.location = ("//" + location.hostname + window.mvcBaseUri + "unsupported_browser.html");
+};
